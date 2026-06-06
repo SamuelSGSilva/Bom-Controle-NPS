efb7c8e296d62f5fdc8e85b37d9c8cdb487cc5f2
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2026-06-05 22:15:55 -0300

    Frontend: telas dashboard, clientes e cadastro de cliente

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from database import engine, Base
 from models import models
 from routes import clientes, pos_venda, nps, auth, relatorios
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bom Controle NPS")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(clientes.router)
