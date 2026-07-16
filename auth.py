b4f95a3e8babe1d410f2ea91cd4d65fcd18b5220
SamuelSGSilva <samuelsantos20021@gmail.com>
2026-07-16 08:07:01 -0300

    Tempo de expirar login aumentado

--- auth.py
+++ auth.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 12  # 12 horas, pra cobrir um turno inteiro de trabalho
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
